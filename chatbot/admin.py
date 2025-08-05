11ab62da094e60139f49c8040ae201223e98f8fc
Bao Dang <baotvdn@gmail.com>
2025-08-05 11:33:56 +0700

    Add admin interfaces and update .gitignore

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib import admin
+
+from chatbot.models import Conversation, Message
+
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "created", "modified"]
+    list_filter = ["created", "modified"]
+    search_fields = ["user__username", "user__email"]
+    ordering = ["-created"]
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["id", "conversation", "role", "content_preview", "created"]
+    list_filter = ["role", "created"]
+    search_fields = ["content"]
+    ordering = ["-created"]
+    
+    def content_preview(self, obj):
+        return obj.content[:50] + "..." if len(obj.content) > 50 else obj.content
+    content_preview.short_description = "Content"
